d665ea6c0915c91e302540414d17999aada225e3
uttambkr <uttambkr@gmail.com>
2021-12-30 21:01:48 +0530

    Update main.tf

--- ecr/module/main.tf
+++ ecr/module/main.tf
@@ -2,7 +2,19 @@ variable "name" {
   type = string
 }
 
+variable "one" {
+  type = string
+}
+
+variable "two" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
 resource "aws_ecr_repository" "demo-repository" {
-  name                 = var.name
+  name                 = var.env == "DEV" ? var.one : {}
   image_tag_mutability = "IMMUTABLE"
 }
