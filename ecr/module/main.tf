b117394e2be1ef4dce9abb9af3cf22ea7ca3215d
uttambkr <uttambkr@gmail.com>
2021-12-30 21:26:17 +0530

    Update main.tf

--- ecr/module/main.tf
+++ ecr/module/main.tf
@@ -16,7 +16,7 @@ variable "env" {
 
 resource "aws_ecr_repository" "demo-repository" {
   for_each = {
-    for one in var.one :
+    for one in var.one
     if var.end != "PROD"
   }
   name                 = each.value
